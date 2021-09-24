7f093ef1deefbf8406a86f239c73a8cc04d302d5
bilal <gkdgbilal>
2021-09-24 15:54:26 +0300

    first commit

---
+++ src/components/Preview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(gif, index) in gifs" :key="index">
+        <img :src="gif.images.fixed_height.url" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["gifs"],
+};
+</script>
+
+<style>
+li {
+  list-style: none;
+  float: left;
+  margin: 20px;
+}
+</style>
